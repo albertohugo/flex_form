a4c375406b551c0524398e1d1216e4301866d0d7
Alberto Monteiro <albertohugo@MBP-de-Alberto.lan>
2020-08-06 21:57:52 +0100

    replace code

--- forms/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Form
-
-class FormAdmin(admin.ModelAdmin):
-    model = Form
-    list_display = ['id','form_title','created_at',]
-
-admin.site.register(Form, FormAdmin)
-
